4130b312369d9206d5360883fbacae914b752fa6
Inajara Pereira <inajara.souza.pereira@gmail.com>
2021-11-10 15:33:24 -0300

    Adicionando produto

--- src/main/java/com/controle_despesa/controller/ProdutoController.java
+++ src/main/java/com/controle_despesa/controller/ProdutoController.java
@@ -4,9 +4,7 @@ import com.controle_despesa.dto.ProdutoDTO;
 import com.controle_despesa.entity.Produto;
 import com.controle_despesa.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,28 @@ public class ProdutoController {
     public List<ProdutoDTO> listaTudo() {
         List<Produto> produtos = produtoRepository.findAll();
         return ProdutoDTO.converter(produtos);
+    }
+
+    @GetMapping("/{id}")
+    public Produto getById(@PathVariable Long id) {
+        return produtoRepository.findById(id).get();
+    }
+
+    @PostMapping
+    public void salvar(@RequestBody Produto produto) {
+        produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualiza(@PathVariable Long id, @RequestBody Produto produto) {
+        Produto produtoRetornado = produtoRepository.getOne(id);
+        produtoRetornado.setValorUnitario(produto.getValorUnitario());
+        produtoRepository.save(produtoRetornado);
     }
 
 }
